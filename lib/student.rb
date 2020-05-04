fbd80a2e3103620ec04652afbc7eec2132e42a10
Erik Prenzlin <eprenzlin@gmail.com>
2020-05-04 20:29:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,8 +40,8 @@ DB[:conn].execute(sql, self.name, self.grade)
 @id = DB[:conn].execute("SELECT LAST_INSERT_ROWID() FROM students")[0][0]
 end
 
-def create(name, grade)
-  student = Student.new(name, grade)
+def create(:name, :grade)
+  student = Student.new(:name, :grade)
   student.save
   student
 end
