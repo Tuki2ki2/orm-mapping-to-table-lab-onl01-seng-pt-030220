92022d5aea2971767d0edaa2e1dcb01f6eee8ba5
Erik Prenzlin <eprenzlin@gmail.com>
2020-05-04 20:29:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ DB[:conn].execute(sql, self.name, self.grade)
 end
 
 def create(:name, :grade)
-  student = Student.new(:name, :grade)
+  student = Student.new(name,grade)
   student.save
   student
 end
